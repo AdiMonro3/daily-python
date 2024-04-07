8b3785974336332b2cdb5eeb93f7a2e0ccf6dd31
AdiMonro3 <adi7ya.t@gmail.com>
2024-04-07 16:17:27 +0530

    calculator

---
+++ calculator.py
@@ -0,0 +1,23 @@
+# create a calculator using python 
+a=float(input("enter here- "))
+b=float(input("\nenter here- "))
+
+
+print("\npress 1 for addition")
+print("press 2 for substraction") 
+print("press 3 for multiplication")
+print("press 4 for division ")
+
+c=int(input("\nenter your choice- " ))
+
+if(c==1): print("\n",a+b)
+
+if(c==2): print("\n",a-b)
+
+if(c==3): print("\n",a*b)
+
+if(c==4): print("\n",a/b)
+
+
+
+
