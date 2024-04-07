21bfd42962603dda771a48f3655466f5594db368
AdiMonro3 <adi7ya.t@gmail.com>
2024-04-07 15:38:27 +0530

    new project starts

---
+++ famous_quote.py
@@ -0,0 +1,5 @@
+# Find a quote from a famous person you admire. Print the quote and the name of its author. Your output should look something like this
+#Albert Einstein once said, “A person who never made a mistake never tried anything new.” 
+
+author_name= "albert einstine"
+print(author_name.title(),'once said ,“A person who never made a mistake never tried anything new.”')
---
+++ personal-messege.py
@@ -0,0 +1,4 @@
+# Use a variable to represent a person’s name, and print a message to that person
+
+name=input("enter your name - " )
+print("hello",name.upper(),"would you like to learn some python")
